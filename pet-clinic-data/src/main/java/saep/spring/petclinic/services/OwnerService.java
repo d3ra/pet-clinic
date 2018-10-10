0740739e2f8dd6ed0181c0f8af3b4abd7bcfff91
Michele Strambi <michele.strambi@saep-ict.it>
2018-10-10 16:55:51 +0200

    Implemented Services

--- pet-clinic-data/src/main/java/saep/spring/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/saep/spring/petclinic/services/OwnerService.java
@@ -2,16 +2,8 @@ package saep.spring.petclinic.services;
 
 import saep.spring.petclinic.model.Owner;
 
-import java.util.Set;
-
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/saep/spring/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/saep/spring/petclinic/services/PetService.java
@@ -2,12 +2,6 @@ package saep.spring.petclinic.services;
 
 import saep.spring.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet owner);
-
-    Set<Pet> findAll();
 }
